76ea6aead9d55774436dfd49b379c29c50804523
vluzko <vincent.luczkow@gmail.com>
2021-11-22 22:24:58 -0500

    Moved more core functions into separate files

--- numts/tensor_core/functional.ts
+++ numts/tensor_core/functional.ts
@@ -6,12 +6,11 @@ import {indexing} from '../indexing';
 /**
  * Map the array.
  * @param f
- * @param {number} axis
  * @return {tensor}
  */
-export function map(f, axis?: number): tensor {
-    const new_data = this.data.map(f);
-    return constructors.array(new_data, this.shape, { disable_checks: true, dtype: this.dtype })
+export function _map(a: tensor, f): tensor {
+    const new_data = a.data.map(f);
+    return constructors.array(new_data, a.shape, { disable_checks: true, dtype: a.dtype })
 }
 
 
@@ -19,20 +18,19 @@ export function map(f, axis?: number): tensor {
  * Accumulating map over the entire array or along a particular axis.
  * If no axis is provided a flat array is returned.
  * Otherwise the shape of the result is the same as the shape of the original array.
- * @param f - Function to use.
+ * @param f - function to use.
  * @param {number} axis - Axis to map over.
  * @param {number} start  - Initial value.
  * @param {string} dtype  - Dtype of the result array.
  * @return {tensor | number}
  */
-export function accum_map(f, axis?: number, start?: number, dtype?: string): tensor | number {
-    dtype = dtype === undefined ? this.dtype : dtype;
+export function _accum_map(a: tensor, f, axis?: number, start?: number, dtype?: string): tensor | number {
+    dtype = dtype === undefined ? a.dtype : dtype;
     let new_array;
     if (axis === undefined) {
         // TODO: Views: Use size of view.
 
-        new_array = constructors.zeros(this.length, dtype);
-        let first_value;
+        new_array = constructors.zeros(a.length, dtype);
 
         if (start !== undefined) {
             new_array.data[0] = start;
@@ -40,31 +38,30 @@ export function accum_map(f, axis?: number, start?: number, dtype?: string): ten
 
         let previous_index = 0;
         let index_in_new = 0;
-        for (let index of this._iorder_data_iterator()) {
-            new_array.data[index_in_new] = f(new_array.data[previous_index], this.data[index]);
+        for (let index of a._iorder_data_iterator()) {
+            new_array.data[index_in_new] = f(new_array.data[previous_index], a.data[index]);
             previous_index = index_in_new;
             index_in_new += 1;
         }
-
     } else {
-        const [lower, upper, steps] = this._slice_for_axis(axis);
-        new_array = constructors.zeros(this.shape, dtype);
-        const step_along_axis = this.stride[axis];
+        const [lower, upper, steps] = a._slice_for_axis(axis);
+        new_array = constructors.zeros(a.shape, dtype);
+        const step_along_axis = a.stride[axis];
 
-        for (let index of this._iorder_data_iterator(lower, upper, steps)) {
+        for (let index of a._iorder_data_iterator(lower, upper, steps)) {
             let first_value;
 
             if (start !== undefined) {
-                first_value = f(start, this.data[index]);
+                first_value = f(start, a.data[index]);
             } else {
-                first_value = this.data[index];
+                first_value = a.data[index];
             }
 
             new_array.data[index] = first_value;
             let previous_index = index;
-            for (let i = 1; i < this.shape[axis]; i++) {
+            for (let i = 1; i < a.shape[axis]; i++) {
                 const new_index = index + i * step_along_axis;
-                new_array.data[new_index] = f(new_array.data[previous_index], this.data[new_index]);
+                new_array.data[new_index] = f(new_array.data[previous_index], a.data[new_index]);
                 previous_index = new_index;
             }
         }
@@ -79,18 +76,18 @@ export function accum_map(f, axis?: number, start?: number, dtype?: string): ten
  * @param {string} dtype
  * @return {tensor | number}
  */
-export function apply_to_axis(f: (a: TypedArray | number[]) => any, axis?: number, dtype?: string): tensor | number {
-    dtype = dtype === undefined ? this.dtype : dtype;
+export function _apply_to_axis(a: tensor, f: (a: TypedArray | number[]) => any, axis?: number, dtype?: string): tensor | number {
+    dtype = dtype === undefined ? a.dtype : dtype;
     if (axis === undefined) {
-        return f(this.data);
+        return f(a.data);
     } else {
-        const new_shape = indexing.new_shape_from_axis(this.shape, axis);
+        const new_shape = indexing.new_shape_from_axis(a.shape, axis);
         let new_array = constructors.zeros(new_shape, dtype);
-        const step_along_axis = this.stride[axis];
-        for (let [old_index, new_index] of this.map_old_indices_to_new(axis)) {
+        const step_along_axis = a.stride[axis];
+        for (let [old_index, new_index] of a.map_old_indices_to_new(axis)) {
             let axis_values = [];
-            for (let i = 0; i < this.shape[axis]; i++) {
-                axis_values.push(this.data[old_index + i * step_along_axis]);
+            for (let i = 0; i < a.shape[axis]; i++) {
+                axis_values.push(a.data[old_index + i * step_along_axis]);
             }
 
             new_array.data[new_index] = f(axis_values);
@@ -106,16 +103,16 @@ export function apply_to_axis(f: (a: TypedArray | number[]) => any, axis?: numbe
  * @param {number} axis
  * @param {string} dtype
  */
-export function reduce(f: (accum: number, e: number, i?: number, array?) => number, initial?: number, axis?: number, dtype?: string): number | tensor {
-    dtype = dtype === undefined ? this.dtype : dtype;
+export function _reduce(a: tensor, f: (accum: number, e: number, i?: number, array?) => number, initial?: number, axis?: number, dtype?: string): number | tensor {
+    dtype = dtype === undefined ? a.dtype : dtype;
     if (axis === undefined) {
-        const iter = this._iorder_value_iterator()[Symbol.iterator]();
+        const iter = a._iorder_value_iterator()[Symbol.iterator]();
         // Deal with initial value
         let { done, value } = iter.next();
         // If it's an empty array, return.
         let accum;
         if (done) {
-            return this;
+            return a;
         } else {
             accum = initial === undefined ? value : f(initial, value);
             while (true) {
@@ -129,13 +126,13 @@ export function reduce(f: (accum: number, e: number, i?: number, array?) => numb
             return accum;
         }
     } else {
-        const new_shape = indexing.new_shape_from_axis(this.shape, axis);
+        const new_shape = indexing.new_shape_from_axis(a.shape, axis);
         let new_array = constructors.zeros(new_shape, dtype);
-        const step_along_axis = this.stride[axis];
-        for (let [old_index, new_index] of this.map_old_indices_to_new(axis)) {
-            let accum = initial === undefined ? this.data[old_index] : f(initial, this.data[old_index]);
-            for (let i = 1; i < this.shape[axis]; i++) {
-                accum = f(accum, this.data[old_index + i * step_along_axis]);
+        const step_along_axis = a.stride[axis];
+        for (let [old_index, new_index] of a.map_old_indices_to_new(axis)) {
+            let accum = initial === undefined ? a.data[old_index] : f(initial, a.data[old_index]);
+            for (let i = 1; i < a.shape[axis]; i++) {
+                accum = f(accum, a.data[old_index + i * step_along_axis]);
             }
 
             new_array.data[new_index] = accum;
--- spec/prop_spec/core/binary_op_prop.spec.js
+++ spec/prop_spec/core/binary_op_prop.spec.js
@@ -1,198 +1,50 @@
-const numts = require('../../numts/numts');
-const binary_ops = require('../../numts/tensor_core/binary_ops');
 const call_python = require('../call_python');
-const tensor = numts.tensor;
+const tensor = require('../../numts/tensor').tensor;
+const binary_ops = require("../../numts/tensor_core/binary_ops");
+const numts = require('../../numts/numts');
+const helpers = require('./helpers');
 
-describe('binary_broadcast.', function() {
-    it('return first.', function () {
-        let a = numts.arange(0, 10);
-        let b = numts.arange(1);
-        let f = (a, b) => a;
-  
-        let broadcasted = binary_ops._binary_broadcast(a, b, f);
-        expect(broadcasted.equals(a)).toBe(true);
-      });
-  
-      it('return second.', function () {
-        let a = numts.arange(1);
-        let b = numts.arange(0, 10);
-        let f = (a, b) => b;
-  
-        let broadcasted = binary_ops._binary_broadcast(a, b, f);
-        expect(broadcasted.equals(b)).toBe(true);
-      });
-  
-      it('take_max', function () {
-        const a = numts.arange(30).reshape(5, 6);
-        const b = numts.arange(30, 60).reshape(5, 6);
-        const c = tensor.take_max(a, b);
-        expect(b.equals(c)).toBe(true);
-      });
-  
-      it('take_min', function () {
-        const a = numts.arange(30).reshape(5, 6);
-        const b = numts.arange(30, 60).reshape(5, 6);
-        const c = tensor.take_min(a, b);
-        expect(a.equals(c)).toBe(true);
-      });
-})
 
-describe('arithmetic.', function () {
-    const a = numts.from_nested_array([[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]], 'int32');
-    const b = numts.arange(1, 6);
-    it('add.', function () {
-        let summed = binary_ops._add(a, b);
-        let expected = numts.from_nested_array([[1, 3, 5, 7, 9], [6, 8, 10, 12, 14]], 'int32');
-        expect(summed.equals(expected)).toBe(true);
-    });
+describe('Basic operations.', () => {
 
-    it('sub.', function () {
-        let sub = binary_ops._sub(a, b);
-        let expected = numts.from_nested_array([[-1, -1, -1, -1, -1], [4, 4, 4, 4, 4]], 'int32');
-        expect(sub.equals(expected)).toBe(true);
-    });
-
-    it('mult.', function () {
-        let product = binary_ops._mult(a, b);
-        let expected = numts.from_nested_array([[0, 2, 6, 12, 20], [5, 12, 21, 32, 45]], 'int32');
-        expect(product.equals(expected)).toBe(true);
-    });
-
-    it('div.', function () {
-        let product = binary_ops._div(a, b);
-        let expected = numts.from_nested_array([
-        [0, 1 / 2, 2 / 3, 3 / 4, 4 / 5],
-        [5, 6 / 2, 7 / 3, 8 / 4, 9 / 5]], 'float64');
-        expect(product.equals(expected)).toBe(true);
-    });
-
-    it('mod.', function () {
-        let product = binary_ops._mod(a, b);
-        let expected = numts.from_nested_array([[0, 1, 2, 3, 4], [0, 0, 1, 0, 4]], 'int32');
-        expect(product.equals(expected)).toBe(true);
-    });
-
-    it('fdiv.', function () {
-        let product = binary_ops._fdiv(a, b);
-        let expected = numts.from_nested_array([[0, 0, 0, 0, 0], [5, 3, 2, 2, 1]], 'int32');
-        expect(product.equals(expected)).toBe(true);
-    });
-
-    it('cdiv.', function () {
-        let product = binary_ops._cdiv(a, b);
-        let expected = numts.from_nested_array([[0, 1, 1, 1, 1], [5, 3, 3, 2, 2]], 'int32');
-        expect(product.equals(expected)).toBe(true);
-    });
-
-    describe('Regression failures.', function () {
-
-        test('_add failure. 2021-08-21', function () {
-            const shape = [1, 4];
-            const a = numts.from_iterable([0, 4.172325134277344e-7, 0.47082263231277466, 5.960464477539062e-7], shape);
-            const b = numts.from_iterable([0, 4.172325134277344e-7, 0.47082263231277466, 5.960464477539062e-7], shape);
-
-            const numts_value = binary_ops._add(a, b);
-
+    function check_op(op) {
+        const f = (a, b) => {
+            const numts_value = binary_ops[op](a, b);
             const a_string = JSON.stringify(a.to_json());
             const b_string = JSON.stringify(b.to_json());
-            const py_str = call_python.call_python('_add', [a_string, b_string])
+            const py_str = call_python.call_python(op, [a_string, b_string])
             const py_value = numts.from_json(JSON.parse(py_str))
 
-            const result = numts_value.is_close(py_value)
-            expect(result.all()).toBe(true);
-        })
+            return numts_value.is_close(py_value).all()
+        };
+        return f;
+    }
+
+    test('Array addition.', () => {
+        helpers.check_arrays(check_op('_add'));
+    });
+
+    test('Array subtraction.', () => {
+        helpers.check_arrays(check_op('_sub'));
     })
-});
 
-describe('boolean.', function () {
+    test('Array multiplication.', () => {
+        helpers.check_arrays(check_op('_mult'));
+    })
 
-})
+    test('Array division.', () => {
+        helpers.check_with_non_zero(check_op('_div'))
+    })
 
-describe('Linalg ops.', function () {
-  describe('matmul_2d.', function () {
-    it('scalar.', function () {
-      let a = numts.arange(1, 2).reshape([1, 1]);
-      let b = numts.arange(10, 11).reshape([1, 1]);
-      let x = tensor.matmul_2d(a, b);
-      const expected = numts.from_nested_array([[10]]);
-      expect(expected.equals(x)).toBe(true);
-    });
+    test('Array ceiling division.', () => {
+        helpers.check_with_non_zero(check_op('_cdiv'))
+    })
 
-    it('2d test', function () {
-      const a = numts.arange(4).reshape(2, 2);
-      const b = numts.arange(4, 8).reshape(2, 2);
-      const expected = numts.from_nested_array([
-        [6, 7],
-        [26, 31]
-      ]);
+    test('Array floor division.', () => {
+        helpers.check_with_non_zero(check_op('_fdiv'))
+    })
 
-      const actual = tensor.matmul_2d(a, b);
-      expect(actual.equals(expected)).toBe(true);
-
-    });
-
-    describe('from breaks.', function() {
-      it('broken broadcast test.', function() {
-        const a = numts.arange(24).reshape(2, 3, 4).slice(0);
-        const b = numts.arange(16).reshape(4, 4);
-        const expected = numts.from_nested_array([
-          [56, 62, 68, 74],
-          [152, 174, 196, 218],
-          [248, 286, 324, 362]
-        ]);
-  
-        const actual = tensor.matmul_2d(a, b);
-        expect(expected.equals(actual)).toBe(true);
-      });
-      
-    });
-  });
-
-  describe('matmul.', function () {
-
-    it('scalar.', function () {
-      let a = numts.arange(1, 2).reshape([1, 1]);
-      let b = numts.arange(10, 11).reshape([1, 1]);
-      let x = binary_ops.broadcast_matmul(a, b);
-      const expected = numts.from_nested_array([[10]]);
-      expect(expected.equals(x)).toBe(true);
-    });
-
-    it('m x n by n by k', function () {
-      let a = numts.arange(15).reshape([5, 3]);
-      let b = numts.arange(12).reshape(3, 4);
-
-      let x = binary_ops.broadcast_matmul(a, b);
-      const expected = numts.from_nested_array([
-
-      ]);
-      expect(expected.equals(x));
-    });
-
-    it('broadcast test.', function () {
-      let a = numts.arange(24).reshape([2, 3, 4]);
-      let b = numts.arange(16).reshape(4, 4);
-
-      let x = binary_ops.broadcast_matmul(a, b);
-      const expected = numts.from_nested_array([
-        [[56, 62, 68, 74],
-        [152, 174, 196, 218],
-        [248, 286, 324, 362]],
-
-        [[344, 398, 452, 506],
-        [440, 510, 580, 650],
-        [536, 622, 708, 794]]
-      ], 'int32');
-      expect(expected.equals(x)).toBe(true);
-    });
-  });
-
-  describe('dot.', function () {
-    it('simple.', function () {
-      const a = numts.arange(10, 20);
-      const b = numts.arange(20, 30);
-      const dot = tensor.dot(a, b);
-      expect(dot).toBe(3635)
-    });
-  });
+    test('Array modulus.', () => {
+        helpers.check_with_non_zero(check_op('_mod'))
+    })
 });
